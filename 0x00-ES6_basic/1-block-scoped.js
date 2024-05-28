333d6a34384c3cd0686af431aefa297dcbfdc44f
Emmanuel Akalazu <138693982+Enetreal@users.noreply.github.com>
2024-05-28 05:49:53 +0100

    0x00-ES6_basic

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
-
 export default function taskBlock(trueOrFalse) {
-    let task = false;
+    const task = false;
     const task2 = true;
   
     if (trueOrFalse) {
-      task = true;
-     
+      // eslint-disable-next-line
+          const task = true;
+      // eslint-disable-next-line
+          const task2 = false;
     }
   
     return [task, task2];
