a87174e2b36f14e64e8c960b6cc32019995d05ca
Emmanuel Akalazu <138693982+Enetreal@users.noreply.github.com>
2024-06-07 12:54:05 +0100

    1-main.js

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,5 @@
+import getListStudnetIds from "./1-getlist_student_ids.js";
+import getListStudnets from "./0-get_list_students.js";
+
+console.log(getListStudentIds("hello"));
+console.log(getListStudentIds(getListStudents()));
